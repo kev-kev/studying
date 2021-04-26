3eb67d123d00d33a3a1204ddf7de57148a5a1b4d
kev-kev <kevin.clark93@gmail.com>
2021-04-25 21:13:18 -0400

    reverse message in place

---
+++ reverse_message_in_place.rb
@@ -0,0 +1,40 @@
+# Write a method reverse_words!() that takes a message as a string and reverses the order of the words in place.
+
+# -- Brainstorming --
+# Easy one liner would be message.split(" ").reverse.join(" "), but that would create a new array 
+# Could iterate through and check for a space- each space would mean a new word
+# But then we would need to create a new message by joining an array
+
+# We already know how to reverse a string in place - 'cake pound steal' becomes 'laets dnuop ekac'
+# We could start by reversing the entire message, then iterate through, looking for spaces, and reversing each word
+def reverse_string!(string, i, j)
+  # i, j = 0, string.length - 1
+  while i < j do
+    string[i], string[j] = string[j], string[i]
+    i += 1
+    j -= 1
+  end
+  string
+end
+
+def reverse_words!(message)
+  reverse_string!(message, 0, message.length - 1).strip!
+  i = 0
+  j = 0 # last space index
+  message.each_char do |c| #'laets dnuop ekac'
+    if c === " " 
+      if message[i + 1] === " " 
+          p message
+          i += 1
+          next
+      end
+      reverse_string!(message, j, i-1)
+      p message
+      j = i + 1
+    end
+      i += 1
+  end
+end
+
+message = "the sky is blue"
+p reverse_words!(message)
