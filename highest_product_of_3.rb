7a3a177feb69bba6ccf1dcf75307d074cf804450
kev-kev <kevin.clark93@gmail.com>
2021-05-02 15:18:55 -0400

    improving highest product of 3

--- highest_product_of_3.rb
+++ highest_product_of_3.rb
@@ -9,11 +9,9 @@
 # but what if we only had two negative ints that could make a large positive product? [1, 10, 5, 15, -20, -25]
 # in that case, we would want both of those negative nums (the two smallest nums) as well as the largest positive num
 
-# here's an adjustede approach:
+# here's an adjusted approach:
 # sort the input arr
-# if the first two nums are negative, we compare their product with the product of the last two nums
-# whichever is 
-
+# if the first two nums are negative, we compare their product with the product of the last two nums...
 
 # def highest_product_of_3(input)
 #   input.sort!
@@ -28,6 +26,8 @@
 #   end
 # end
 
+# but really, that is the same as writing this...
+
 # def highest_product_of_3(input)
 #   input.sort!
 #   product1 = input[0] * input[1]
@@ -37,11 +37,37 @@
 #     product2 * input[-3]
 # end
 
+# which is more concise, but we have an overlooked issue- what if input[-3] is a negative num? then product1 would be larger
+# it also would make way more sense to keep all the math in one place... so let's just do it all at once and compare the products
+
+# def highest_product_of_3(input)
+#   input.sort!
+#   product1 = input[0] * input[1] * input[-1]
+#   product2 = input[-1] * input[-2] * input[-3]
+#   [product1, product2].max
+# end
+
+# looks better, but maybe we can do this without the built in sort method (O(lgn))
+# we can get the largest num one of two ways: multiplying the most negative number by a negative int, or multiplying the most positive number by a positive int
+# a new approach could then be:
+# keep track of the max and min products of two in the arr (can set both equal to input[0]*input[1]), the highest and lowest ints in the input, and the highest product of 3 so far
+# each num in input[2..-1], check if it's the highest/lowest num, if it can be multiplied with the highest/lowest num to make the highest/lowest product of 2, and check if it can be multiplied by the highest/lowest product of 2 to be the largest so far
+# then, return the largest value so far as the result ==> O(n) time 
+
 def highest_product_of_3(input)
-  input.sort!
-  product1 = input[0] * input[1] * input[-1]
-  product2 = input[-1] * input[-2] * input[-3]
-  [product1, product2].max
+  min = [input[0], input[1]].min
+  max = [input[0], input[1]].max
+  min_product = (input[0] * input[1])
+  max_product = (input[0] * input[1])
+  result = input[0] * input[1] * input[2]
+  input[2..-1].each do |n|
+    result = [result, n * min_product, n * max_product].max
+    min_product = [n * min, n * max, min_product].min
+    max_product = [n * max, n * min, max_product].max
+    min = n if n < min
+    max = n if n > max
+  end
+  result
 end
 
 testcase = [-8,-7,-2,10,20] #1120
